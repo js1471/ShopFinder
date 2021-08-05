bf672839343b3d2c8c55e351efd80ddb97276a8e
Karandeep Singh <KS061@atcltd.atc.org.nz>
2021-08-06 10:47:24 +1200

    Add project files.

---
+++ ShopFinder/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using ShopFinder.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShopFinder.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Section> Sections { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
